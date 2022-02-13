64e58ef5d2e7288cbc2c45fa259945469f3a6fad
Patrick El-Azem <p@elazem.com>
2022-02-13 13:51:14 -0500

    Write new key files to user ssh folder

--- scripts/02-ssh/02-prepare-ssh-info.sh
+++ scripts/02-ssh/02-prepare-ssh-info.sh
@@ -10,25 +10,25 @@ expirationDate="$(date +%s -d "$(date) + 1 year")"
 vmAdminSshPublicKeyInfix="AAAAB3NzaC1yc2EAAAABJQAAAQEAg+4FzJlW5nqUa798vqYGanooy5HvSyG8sS6KjPu0sJAf+fkP6qpHY8k1m2/Z9Mahv2Y0moZDiVRHFMGH8qZU+AlYdvjGyjxHcIzDnsmHcV2ONxEiop4KMJLwecHUyf95ogicB1QYfK/6Q8pL9sDlXt8bAcSh6iP0u2d1g9QJaON2aniOpzn68xnKdGT974i7JQLN0SjaPiidZ2prc0cSIMBN26tGV7at2Jh5FIb1Jv8fXHnZebD/vgLilfCqLbuQjTpDVCskZ+OUAyvlBko3gBjRgd/jBprMqCpFLoGUBVkSSR0IkjTj2A6n2XyCyYRMFYrVrjwyU8I+IvO/6zJSEw=="
 vmAdminSshPublicKey="ssh-rsa ""$vmAdminSshPublicKeyInfix"" ""$VM_ADMIN_SSH_USER_NAME"
 
-#echo "Write VM Admin Username to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-Username" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_USER_NAME" \
-	secretValue="$VM_ADMIN_SSH_USER_NAME" \
-	expirationDate="$expirationDate"
+##echo "Write VM Admin Username to Key Vault"
+#az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-Username" --verbose \
+#	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
+#	--parameters \
+#	location="$LOCATION" \
+#	keyVaultName="$KEYVAULT_NAME" \
+#	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_USER_NAME" \
+#	secretValue="$VM_ADMIN_SSH_USER_NAME" \
+#	expirationDate="$expirationDate"
 
-#echo "Write VM Admin User SSH Public Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-PubKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_SSH_PUBLIC_KEY" \
-	secretValue="$vmAdminSshPublicKey" \
-	expirationDate="$expirationDate"
+##echo "Write VM Admin User SSH Public Key to Key Vault"
+#az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-PubKey" --verbose \
+#	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
+#	--parameters \
+#	location="$LOCATION" \
+#	keyVaultName="$KEYVAULT_NAME" \
+#	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_SSH_PUBLIC_KEY" \
+#	secretValue="$vmAdminSshPublicKey" \
+#	expirationDate="$expirationDate"
 
 
 # Deploy user SSH public and private keys
@@ -36,6 +36,9 @@ az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-Pub
 # about this SSH user (plus of course we configure the VM admin SSH public key below). This eliminates the risk of credential leak: the SSH user and keys
 # are generated here - this could be your dev box, or a build server, or a CI/CD pipeline agent - and are as transient as you want.
 # If this is a hosted build agent, this SSH public/private key pair's lifetime is only as long as the build agent's (plus a cleanup script is provided).
+# Below we write the public AND private keys to Key Vault. The private key is only needed if it will be used again in a later deployment or other context.
+# If the deployment SSH key will ONLY be used in a single context - e.g. a single pipeline execution - and will not be needed again later, the private key does not
+# need to be written to Key Vault. Determine whether the private key should be persisted to Key Vault based on your circumstances and constraints.
 
 sshKeyName="ssh-vm-deploy"
 sshKeyType="ecdsa"
@@ -43,53 +46,65 @@ sshKeyBits=521
 sshKeyPassphrase="" # Use blank for convenience here as SSH key will be short-lived
 sshPublicKeyUsername="$DEPLOYMENT_SSH_USER_NAME"
 
+# Clean up existing key files here, if any
+delCmd="rm ./""$sshKeyName""*"
+eval $delCmd
+
+# Generate new public and private key pair and write the files here
 ssh-keygen -q -f "./""$sshKeyName" -t "$sshKeyType" -b $sshKeyBits -N "$sshKeyPassphrase" -C "$sshPublicKeyUsername"
 
-echo "Copy new SSH pub/priv key files to ~/.ssh"
-mkdir ~/.ssh
-cp "./""$sshKeyName" ~/.ssh
-cp "./""$sshKeyName"".pub" ~/.ssh
+# Read keys from file into variables
+privateKey=$(<$sshKeyName)
+echo $privateKey
+publicKey=$(<"$sshKeyName"".pub")
+echo $publicKey
 
-echo "Set SSH key file permissions"
+# Move new SSH pub/priv key files to ~/.ssh"
+mkdir ~/.ssh
+mv "./""$sshKeyName" ~/.ssh
+mv "./""$sshKeyName"".pub" ~/.ssh
+
+# Set SSH key file permissions"
 privCmd="sudo chmod 600 ~/.ssh/""$sshKeyName"
 eval $privCmd
 pubCmd="sudo chmod 644 ~/.ssh/""$sshKeyName"".pub"
 eval $pubCmd
 
-echo "Add SSH key to SSH agent"
+# Add SSH key to SSH agent
 eval $(ssh-agent)
 sudo ssh-add
 
 # Cleanup
 # sudo ssh-add -d "~/.ssh/""$sshKeyName"
+# rm "~./ssh/""$sshKeyName""*"
 
 
-#echo "Write Deployment Username to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-Username" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_USER_NAME" \
-	secretValue="$DEPLOYMENT_SSH_USER_NAME" \
-	expirationDate="$expirationDate"
+##echo "Write Deployment Username to Key Vault"
+#az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-Username" --verbose \
+#	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
+#	--parameters \
+#	location="$LOCATION" \
+#	keyVaultName="$KEYVAULT_NAME" \
+#	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_USER_NAME" \
+#	secretValue="$DEPLOYMENT_SSH_USER_NAME" \
+#	expirationDate="$expirationDate"
 
-#echo "Write Deployment User SSH Public Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PubKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PUBLIC_KEY" \
-	secretValue="$DEPLOYMENT_SSH_PUBLIC_KEY" \
-	expirationDate="$expirationDate"
+##echo "Write Deployment User SSH Public Key to Key Vault"
+#az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PubKey" --verbose \
+#	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
+#	--parameters \
+#	location="$LOCATION" \
+#	keyVaultName="$KEYVAULT_NAME" \
+#	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PUBLIC_KEY" \
+#	secretValue="$DEPLOYMENT_SSH_PUBLIC_KEY" \
+#	expirationDate="$expirationDate"
 
-#echo "Write Deployment User SSH Private Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PrivKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PRIVATE_KEY" \
-	secretValue="$DEPLOYMENT_SSH_PRIVATE_KEY" \
-	expirationDate="$expirationDate"
+##echo "Write Deployment User SSH Private Key to Key Vault"
+#az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PrivKey" --verbose \
+#	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
+#	--parameters \
+#	location="$LOCATION" \
+#	keyVaultName="$KEYVAULT_NAME" \
+#	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PRIVATE_KEY" \
+#	secretValue="$DEPLOYMENT_SSH_PRIVATE_KEY" \
+#	expirationDate="$expirationDate"
--- scripts/02-ssh/03-write-secrets-to-key-vault.sh
+++ scripts/02-ssh/03-write-secrets-to-key-vault.sh
@@ -1,57 +1,5 @@
 #!/bin/bash
 
 
-echo "Set Secret expiration date to 1 year from today - adjust as needed"
-expirationDate="$(date +%s -d "$(date) + 1 year")"
 
 
-#echo "Write Deployment Username to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-Username" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_USER_NAME" \
-	secretValue="$DEPLOYMENT_SSH_USER_NAME" \
-	expirationDate="$expirationDate"
-
-#echo "Write Deployment User SSH Public Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PubKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PUBLIC_KEY" \
-	secretValue="$DEPLOYMENT_SSH_PUBLIC_KEY" \
-	expirationDate="$expirationDate"
-
-#echo "Write Deployment User SSH Private Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-Deploy-PrivKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_DEPLOYMENT_SSH_PRIVATE_KEY" \
-	secretValue="$DEPLOYMENT_SSH_PRIVATE_KEY" \
-	expirationDate="$expirationDate"
-
-
-#echo "Write VM Admin Username to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-Username" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_USER_NAME" \
-	secretValue="$VM_ADMIN_SSH_USER_NAME" \
-	expirationDate="$expirationDate"
-
-#echo "Write VM Admin User SSH Public Key to Key Vault"
-az deployment group create --subscription "$SUBSCRIPTION_ID" -n "KV-VM-Admin-PubKey" --verbose \
-	-g "$RG_NAME_SECURITY" --template-uri "$TEMPLATE_KEYVAULT_SECRET" \
-	--parameters \
-	location="$LOCATION" \
-	keyVaultName="$KEYVAULT_NAME" \
-	secretName="$KEYVAULT_SECRET_NAME_VM_ADMIN_SSH_PUBLIC_KEY" \
-	secretValue="$VM_ADMIN_SSH_PUBLIC_KEY" \
-	expirationDate="$expirationDate"
