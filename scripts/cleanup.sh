0ab44114785bd609d216e1e043ca121caa901a4c
Patrick El-Azem <p@elazem.com>
2022-02-18 09:12:05 -0500

    Last SSH fixes:

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# WARNING - this will delete ALL resources in all of these Resource Groups
+# Cleanup SSH entries
+./ssh-cleanup.sh
 
+# WARNING - this will delete ALL resources in all of these Resource Groups
 az group delete --subscription "$SUBSCRIPTION_ID" -n "$RG_NAME_DEPLOY" --yes --verbose
 az group delete --subscription "$SUBSCRIPTION_ID" -n "$RG_NAME_SOURCE" --yes --verbose
 az group delete --subscription "$SUBSCRIPTION_ID" -n "$RG_NAME_SIG" --yes --verbose
 az group delete --subscription "$SUBSCRIPTION_ID" -n "$RG_NAME_SECURITY" --yes --verbose
 az group delete --subscription "$SUBSCRIPTION_ID" -n "$RG_NAME_NET" --yes --verbose
-
-# Cleanup SSH entries
-./ssh-cleanup.sh
