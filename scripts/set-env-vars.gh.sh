ee3c430903ba6e14f4e47b4e6d7eba1e599637ff
Patrick El-Azem <p@elazem.com>
2022-02-24 06:48:25 -0500

    Test dynamic env vars with custom action

--- scripts/set-env-vars.gh.sh
+++ scripts/set-env-vars.gh.sh
@@ -2,5 +2,5 @@
 
 subscriptionName="Sandbox"
 
-EV1=$(echo "$(az group show --subscription $subscriptionName -n 'azq' -o tsv --query 'location')" | sed "s/\r//")
-echo "EV1=$EV1" >> $GITHUB_ENV
+EV17=$(echo "$(az group show --subscription $subscriptionName -n 'azq' -o tsv --query 'location')" | sed "s/\r//")
+echo "EV17=$EV17" >> $GITHUB_ENV
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Testing"
-echo ""
-echo $EV1
+echo "EV17"
+echo $EV17
