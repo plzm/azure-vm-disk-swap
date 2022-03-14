e6f703d7730ca50ab67be34dc13b0415aab420e2
Patrick El-Azem <p@elazem.com>
2022-03-14 06:17:33 -0400

    Update 01 pipeline

--- scripts/01-deploy-infra/05-deploy-sig.sh
+++ scripts/01-deploy-infra/05-deploy-sig.sh
@@ -10,5 +10,11 @@
 
 echo "Create Azure Compute Gallery"
 # https://docs.microsoft.com/cli/azure/sig?view=azure-cli-latest#az_sig_create
-az sig create --subscription "$SUBSCRIPTION_ID" -g "$RG_NAME_SIG" -l "$LOCATION" --verbose \
-	--gallery-name "$SIG_NAME" # --soft-delete false
+#az sig create --subscription "$SUBSCRIPTION_ID" -g "$RG_NAME_SIG" -l "$LOCATION" --verbose \
+#	--gallery-name "$SIG_NAME" # --soft-delete false
+
+az deployment group create --subscription "$SUBSCRIPTION_ID" -n "CG-""$LOCATION" --verbose \
+	-g "$RG_NAME_SIG" --template-uri "$TEMPLATE_COMPUTE_GALLERY" \
+	--parameters \
+	location="$LOCATION" \
+	gallery-name="$SIG_NAME"
