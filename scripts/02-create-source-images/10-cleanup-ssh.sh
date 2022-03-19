62198789d736d61fdcb1dcceebf051d179d3adc3
Patrick El-Azem <p@elazem.com>
2022-03-19 17:44:46 -0400

    Complete pipeline 2

--- scripts/02-create-source-images/10-cleanup-ssh.sh
+++ scripts/02-create-source-images/10-cleanup-ssh.sh
@@ -7,18 +7,8 @@ sshAddCmd="ssh-add -d ~/.ssh/""$DEPLOYMENT_SSH_USER_KEY_NAME"
 eval $sshAddCmd
 
 # Delete existing SSH key files, if any
-delCmd="rm ./""$DEPLOYMENT_SSH_USER_KEY_NAME""*"
-#echo $delCmd
-eval $delCmd
-
 delCmd="rm ~/.ssh/""$DEPLOYMENT_SSH_USER_KEY_NAME""*"
-#echo $delCmd
-eval $delCmd
-
-delCmd="rm ./""$VM_ADMIN_SSH_USER_KEY_NAME""*"
-#echo $delCmd
 eval $delCmd
 
 delCmd="rm ~/.ssh/""$VM_ADMIN_SSH_USER_KEY_NAME""*"
-#echo $delCmd
 eval $delCmd
