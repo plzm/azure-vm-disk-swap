8fd6be1acd9984504f89ab3c123fb45e2f955852
Patrick El-Azem <p@elazem.com>
2022-03-19 16:43:43 -0400

    Update source VM prep script

--- scripts/02-create-source-images/remote-cmd.sh
+++ scripts/02-create-source-images/remote-cmd.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # This is the script to run remotely on source VMs
 
-touch i_was_here.txt
-
 sudo mkdir /patrick_was_here
-
-#sudo chown -R root:root /patrick_was_here
+sudo chown -R root:root /patrick_was_here  # This is so it doesn't get deleted as part of user deprovisioning
 
 # Other configuration etc. etc.
 
-#sudo waagent -deprovision+user -force
+sudo waagent -deprovision+user -force
