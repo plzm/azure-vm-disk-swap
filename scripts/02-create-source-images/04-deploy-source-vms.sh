1953ba85252f5e16a4ffd6d8efe876800322eb83
Patrick El-Azem <p@elazem.com>
2022-03-22 08:35:36 -0400

    Troubleshoot source VM deploy:

--- scripts/02-create-source-images/04-deploy-source-vms.sh
+++ scripts/02-create-source-images/04-deploy-source-vms.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -eux
 
 keyFilePath=~/.ssh/"$DEPLOYMENT_SSH_USER_KEY_NAME".pub
 vmDeploySshPublicKey=$(<$keyFilePath)
+echo $vmDeploySshPublicKey
 
 echo "Deploy Source VMs to use for image capture"
 
