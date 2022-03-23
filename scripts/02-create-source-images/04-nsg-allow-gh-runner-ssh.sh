e29d9cee513993f4963a94a3d20fa8c3edfc56a5
Patrick El-Azem <p@elazem.com>
2022-03-23 07:33:17 -0400

    Update source SSH scripts with relative path

--- scripts/02-create-source-images/04-nsg-allow-gh-runner-ssh.sh
+++ scripts/02-create-source-images/04-nsg-allow-gh-runner-ssh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+scriptdir="$(dirname "$0")"
+cd "$scriptdir"
+
 ../ssh/04-nsg-allow-gh-runner-ssh-vnet.sh
 
 ../ssh/06-nsg-allow-gh-runner-ssh-vm.sh "$RG_NAME_VM_SOURCE" "$VM_SRC_NAME_V2" "$NSG_RULE_NAME_GH_SOURCE_VM_V2" "$NSG_RULE_PRIORITY_GH_SOURCE_VM_V2"
