8b7ada5bddd32f99cc09e112c3d58a39f35504a4
Patrick El-Azem <p@elazem.com>
2022-03-24 07:00:27 -0400

    Debug GH runner SSH access to deployed VM

--- scripts/03-deploy-prod-vms/03-nsg-allow-gh-runner-ssh.sh
+++ scripts/03-deploy-prod-vms/03-nsg-allow-gh-runner-ssh.sh
@@ -5,7 +5,7 @@ cd "$scriptdir"
 
 echo "Add NSG rules to allow SSH from GitHub runner"
 
-#../ssh/04-nsg-allow-gh-runner-ssh-vnet.sh
+../ssh/04-nsg-allow-gh-runner-ssh-vnet.sh
 
-../ssh/06-nsg-allow-gh-runner-ssh-vm.sh "$RG_NAME_VM_PROD" "$VM_PROD_NAME_1" "$NSG_RULE_NAME_GH_PROD_VM_1" "$NSG_RULE_PRIORITY_GH_PROD_VM_1"
-../ssh/06-nsg-allow-gh-runner-ssh-vm.sh "$RG_NAME_VM_PROD" "$VM_PROD_NAME_2" "$NSG_RULE_NAME_GH_PROD_VM_2" "$NSG_RULE_PRIORITY_GH_PROD_VM_2"
+#../ssh/06-nsg-allow-gh-runner-ssh-vm.sh "$RG_NAME_VM_PROD" "$VM_PROD_NAME_1" "$NSG_RULE_NAME_GH_PROD_VM_1" "$NSG_RULE_PRIORITY_GH_PROD_VM_1"
+#../ssh/06-nsg-allow-gh-runner-ssh-vm.sh "$RG_NAME_VM_PROD" "$VM_PROD_NAME_2" "$NSG_RULE_NAME_GH_PROD_VM_2" "$NSG_RULE_PRIORITY_GH_PROD_VM_2"
