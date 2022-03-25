b3e2bf953f06954fd95ec20f0fc569f28a987253
Patrick El-Azem <p@elazem.com>
2022-03-25 06:52:24 -0400

    Minor image naming fixes

--- scripts/04-create-os-disk-from-image/01-create-os-disk-from-image.sh
+++ scripts/04-create-os-disk-from-image/01-create-os-disk-from-image.sh
@@ -7,7 +7,7 @@ sigImageRefVNext=$(echo "$(az sig image-version show --subscription "$SUBSCRIPTI
 # Create managed OS disks from SIG image versions
 # https://docs.microsoft.com/cli/azure/disk?view=azure-cli-latest#az_disk_create
 
-echo "Create VM 1 OS Disk vNext"
+echo "Create VM1 OS Disk vNext"
 az disk create --subscription "$SUBSCRIPTION_ID" -g "$RG_NAME_VM_PROD" -l "$LOCATION" --verbose \
 	-n "$VM_1_OS_DISK_NAME_VNEXT" --gallery-image-reference "$sigImageRefVNext" \
 	--os-type "$VM_OS_TYPE" --sku "$OS_DISK_STORAGE_TYPE"
