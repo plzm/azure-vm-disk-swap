d6de39cd2cebcf772a3e4aae34b757fd1c0ddbfc
Patrick El-Azem <p@elazem.com>
2022-03-26 08:22:35 -0400

    Troubleshoot waagent deprovision

--- scripts/03-create-source-image/remote-cmd.sh
+++ scripts/03-create-source-image/remote-cmd.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 # This is the script to run remotely on source VMs
 
-sudo apt update -y
-sudo apt upgrade -y
-
-sudo apt install 
-
-
 sudo mkdir /patrick_was_here
 sudo touch /patrick_was_here/foo.txt
 
-sudo chown -v -R root /patrick_was_here/  # This is so it doesn't get deleted as part of user deprovisioning
-sudo chmod 744 /patrick_was_here/
+#sudo chown -v -R root /patrick_was_here/  # This is so it doesn't get deleted as part of user deprovisioning
+#sudo chmod 744 /patrick_was_here/
 
 # Other configuration etc. etc.
 
 #sudo waagent -deprovision+user -force -verbose
-sudo waagent -deprovision -force
+#sudo waagent -deprovision -force -verbose
