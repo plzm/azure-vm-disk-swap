865fa7c2737d2d3f07411927f4e64b3255bb9800
Patrick El-Azem <p@elazem.com>
2022-03-26 11:31:32 -0400

    Troubleshoot waagent deprovision

--- scripts/03-create-source-image/remote-cmd.sh
+++ scripts/03-create-source-image/remote-cmd.sh
@@ -8,10 +8,9 @@ sudo apt install nginx -y
 sudo mkdir /usr/patrick_was_here
 sudo touch /usr/patrick_was_here/foo.txt
 
-#sudo chown -v -R root /patrick_was_here/  # This is so it doesn't get deleted as part of user deprovisioning
-#sudo chmod 744 /patrick_was_here/
+sudo chown -v -R root /patrick_was_here/
+sudo chmod 744 /patrick_was_here/
 
 # Other configuration etc. etc.
 
-#sudo waagent -deprovision+user -force -verbose
-sudo waagent -deprovision -force -verbose
+sudo waagent -deprovision+user -force -verbose
