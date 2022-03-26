15cfb85d880afa29bcadb74f056b084e0d657c07
Patrick El-Azem <p@elazem.com>
2022-03-26 06:59:14 -0400

    Debug source image deprovision

--- scripts/03-create-source-image/remote-cmd.sh
+++ scripts/03-create-source-image/remote-cmd.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # This is the script to run remotely on source VMs
 
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install 
+
+
 sudo mkdir /patrick_was_here
 sudo touch /patrick_was_here/foo.txt
 
-sudo chown -R root:root /patrick_was_here  # This is so it doesn't get deleted as part of user deprovisioning
+sudo chown -v -R root /patrick_was_here/  # This is so it doesn't get deleted as part of user deprovisioning
+sudo chmod 744 /patrick_was_here/
 
 # Other configuration etc. etc.
 
-#sudo waagent -deprovision+user -force
+#sudo waagent -deprovision+user -force -verbose
 sudo waagent -deprovision -force
